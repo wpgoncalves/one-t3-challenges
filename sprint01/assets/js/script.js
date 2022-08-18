1d263722f91ca78e08fe752bbbbe42435578a5c3
Wesley Paulo Gonçalves <wpgoncalves1984@gmail.com>
2022-08-18 01:27:34 -0300

    Visual modifications of components and styles. Finalization of the operating logic.

--- sprint01/assets/js/script.js
+++ sprint01/assets/js/script.js
@@ -1,21 +1,54 @@
-// var word = "GATO gato gatinho Gato Avião";
+const textareaInput = document.querySelector("#input-content");
+const textareaOutput = document.querySelector("#output-content");
+const outputEmpty = document.querySelector(".output-empty");
+const outputFound = document.querySelector(".output-found");
+const btnEncrypt = document.querySelector(".btn-encrypt");
+const btnDecrypt = document.querySelector(".btn-decrypt");
+const btnCopy = document.querySelector(".btn-copy");
 
-// console.log(word);
-// console.log(encrypt(word));
-// console.log(decrypt(encrypt(word)));
+btnEncrypt.addEventListener("click", () => {
+    let value = textareaInput.value;
+    let isEncrypted = value.search(/ai|enter|imes|ober|ufat/i) < 0 ? false : true;
 
-const textarea = document.querySelector("textarea");
+    if (value !== "" && !isEncrypted) {
+        outputEmpty.style.display = "none";
+        outputFound.style.display = "flex";
+        textareaOutput.value = encrypt(value);
+    } else {
+        outputEmpty.style.display = "flex";
+        outputFound.style.display = "none";
+        alert("É necessário informar um texto plano para realizar a encriptação do conteúdo!");
+    }
 
-textarea.addEventListener("keyup", e => {
-    textarea.style.height = "auto";
-
-    let scHeight = e.target.scrollHeight / 16;
-    console.log(scHeight);
-    textarea.style.height = `${scHeight}rem`;
+    textareaInput.focus();
 });
 
+btnDecrypt.addEventListener("click", () => {
+    let value = textareaInput.value;
+    let isEncrypted = value.search(/ai|enter|imes|ober|ufat/i) < 0 ? false : true;
+
+    if (value !== "" && isEncrypted) {
+        outputEmpty.style.display = "none";
+        outputFound.style.display = "flex";
+        textareaOutput.value = decrypt(value);
+    } else {
+        outputEmpty.style.display = "flex";
+        outputFound.style.display = "none";
+        alert("É necessário informar um texto encriptado para realizar a desencriptação do conteúdo!");
+    }
+
+    textareaInput.focus();
+});
+
+btnCopy.addEventListener("click", clipboardCopy);
+
+async function clipboardCopy(){
+    let content = textareaOutput.value;
+    await navigator.clipboard.writeText(content);
+}
+
 function encrypt(value) {
-    return value.replace(/[aeiou]/gi, expr => {
+    return value.replace(/[aeiou]/gi, (expr) => {
 
         let isUpper = /[A-Z]/.test(expr);
 
@@ -35,7 +68,7 @@ function encrypt(value) {
 }
 
 function decrypt(value) {
-    return value.replace(/ai|enter|imes|ober|ufat/gi, expr => {
+    return value.replace(/ai|enter|imes|ober|ufat/gi, (expr) => {
 
         let isUpper = /[A-Z]/.test(expr);
 
